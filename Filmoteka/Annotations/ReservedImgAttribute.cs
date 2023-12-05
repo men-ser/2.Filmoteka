c56ce6c1481243b9184a503e0bcdda98a2172c3d
men-ser <sergii.meniailo@gmail.com>
2023-12-05 12:13:06 +0200

    #13 Добавлен свой атрибут валидации

---
+++ Filmoteka/Annotations/ReservedImgAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Filmoteka.Annotations
+{
+    public class ReservedImgAttribute : ValidationAttribute
+    {
+        private static string[] myReservedImg;
+
+        public ReservedImgAttribute(string[] Imgs)
+        {
+            myReservedImg = Imgs;
+        }
+
+        public override bool IsValid(object value)
+        {
+            if (value != null)
+            {
+                string temp = value.ToString();
+                for (int i = 0; i < myReservedImg.Length; i++)
+                {
+                    if (temp == myReservedImg[i])
+                        return false;
+                }
+            }
+            return true;
+
+        }
+    }
+}
